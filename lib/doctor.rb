0d8048069700546b15de8d465536fa1d329bf72f
rosa lee <rosalee0116@gmail.com>
2020-04-21 23:12:39 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,27 +1,35 @@
 class Doctor
 
     attr_accessor :name
-   @@all = []
+
+  @@all = []
 
     def initialize(name)
      @name = name
+     @patients = []
      @@all << self
-   end
-
-    def appointments
-     Appointment.all.select {|appointment| appointment.doctor == self}
-   end
-
-    def patients
-     appointments.map {|appointment| appointment.patient}
    end
 
     def self.all
      @@all
    end
 
-    def new_appointment(patient, date)
-     Appointment.new(patient, date, self)
-   end
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+ end
 
-  end 
+ 
+  def appointments
+   Appointment.all.select {|apt| apt.doctor == self}
+ end
+
+ 
+  def patients
+   #binding.pry
+   appointments.each do |apt|
+   @patients << apt.patient
+ end
+ @patients
+ end
+
+  end
