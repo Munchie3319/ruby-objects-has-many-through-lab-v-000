f8a96f247973d30757454efee2b264066cf916b4
rosa lee <rosalee0116@gmail.com>
2020-04-21 22:15:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,16 +4,16 @@ class Patient
   @@all = [ ]
 
   def initialize(name)
-    @name = name 
-    @@all << self 
+    @name = name
+    @@all << self
   end
 
   def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
   def appointments
