78cc1086b86999e545a9ba4db28b9f9c0c4b1bb4
rosa lee <rosalee0116@gmail.com>
2020-04-21 22:50:47 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,32 +1,29 @@
-class Doctor
+class Doctor 
 
-  attr_accessor :name
+    attr_reader :name 
 
-  @@all = []
+    @@all = []
 
-  def initialize(name)
-   @name = name
-   @@all << self
- end
+    def initialize(name)
+     @name = name 
+     @@all << self 
+   end 
 
-  def self.all
-   @@all
- end
+    def self.all 
+     @@all 
+   end 
 
-  def appointments
-   Appointment.all.select do |appointment|
-     appointment.doctor == self
+    def appointments
+     Appointment.all.select {|appointment| appointment.doctor}
    end
- end
 
-  def new_appointment(patient, date)
-  Appointment.new(date, patient, self)
- end
-
-  def patients
-   appointments.map do |appointment|
-     appointment.patient
+    def patients
+     appointments.map {|appointment| appointment.patient}
    end
- end
 
-  end
+ 
+    def new_appointment(date, patient)
+     Appointment.new(date, patient, self)
+   end
+
+  end 
