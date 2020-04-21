91f7c9ca9fac53412d665a197d9738d52b83c653
rosa lee <rosalee0116@gmail.com>
2020-04-21 22:07:56 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,17 @@
-class Appointment 
+class Appointment
 
   attr_accessor :date, :patient, :doctor
 
   @@all = [ ]
 
   def initialize(date, patient, doctor)
-    @date = date 
-    @patient = patient 
-    @doctor = doctor 
+    @date = date
+    @patient = patient
+    @doctor = doctor
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 end
