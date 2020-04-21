a7a31b1232be59df9918dc359e1861ddc69bf749
rosa lee <rosalee0116@gmail.com>
2020-04-21 23:15:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,35 +1,30 @@
 class Doctor
+     attr_accessor :name
 
-    attr_accessor :name
+      @@all = [ ]
 
-  @@all = []
+      def self.all
+         @@all
+     end
 
-    def initialize(name)
-     @name = name
-     @patients = []
-     @@all << self
-   end
+      def initialize(name)
+         @name = name
+         @@all << self
+     end
 
-    def self.all
-     @@all
-   end
+      def new_appointment(patient, date)
+         Appointment.new(date, patient, self)
+     end
 
-  def new_appointment(patient, date)
-    Appointment.new(date, patient, self)
- end
+      def appointments
+         Appointment.all.select {|a| a.doctor == self}
+     end
 
+      def patients
+         appointments.collect {|a| a.patient}
+     end
 
-  def appointments
-   Appointment.all.select {|apt| apt.doctor == self}
- end
-
-
-  def patients
-   #binding.pry
-   appointments.each do |apt|
-   @patients << apt.patient
- end
- @patients
- end
-
+ 
+ 
   end
+
