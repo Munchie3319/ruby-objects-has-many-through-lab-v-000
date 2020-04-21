4e734cadd19a24e9244ef52395d37a99737d912a
rosa lee <rosalee0116@gmail.com>
2020-04-21 22:40:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,26 @@
 class Artist
-  attr_accessor :name
+   attr_accessor :name
 
-  @@all = [ ]
+    @@all = []
 
-  def self.all
-    @@all
-  end
+    def initialize(name)
+     @name = name
+     @@all << self
+   end
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+    def self.all
+     @@all
+   end
 
-  def new_song(name, genre)
-    Song.new(name, self, genre)
-  end
+    def new_song(name, genre)
+     Song.new(name, self, genre)
+   end
 
-  def songs
-    Song.all.select{|song| song.artist == self}
-  end
+    def songs
+     Song.all.select {|song| song.artist == self}
+   end
 
-  def genres
-    songs.collect{|song| song.genre}
-  end
-end
+    def genres
+     songs.map { |song| song.genre  }
+   end
+ end
